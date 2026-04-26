124f89096d635222f0e7875e1a8afea25e8aaef7
Armin <mehri.armin@gmail.com>
2026-04-26 13:26:52 +0200

    feat(web): annotation import dialog with progress + warnings

--- apps/web/src/routes/projects.$projectId.tasks.$taskId.tsx
+++ apps/web/src/routes/projects.$projectId.tasks.$taskId.tsx
@@ -3,6 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 import { rootRoute } from "./_root";
 import { RequireAuth } from "@/auth/RequireAuth";
 import { AssetUploadDialog } from "@/pages/AssetUploadDialog";
+import { ImportDialog } from "@/pages/ImportDialog";
 import { AssetGrid } from "@/pages/AssetGrid";
 import { projectsApi } from "@/api/projects";
 import { tasksApi } from "@/api/tasks";
@@ -31,6 +32,7 @@ function TaskDetail() {
           )}
         </header>
         <AssetUploadDialog projectId={projectId} taskId={taskId} />
+        <ImportDialog taskId={taskId} />
         <AssetGrid projectId={projectId} taskId={taskId} />
       </div>
     </RequireAuth>
